c0e1c39b0f716f53150b6394e1f4cf3185dd8b49
pzl111 <phuazailian@gmail.com>
2025-03-06 21:09:31 +0800

    feat: Implement get all users

--- client/src/pages/admin/Users.js
+++ client/src/pages/admin/Users.js
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 import Layout from '../../components/Layout';
 import AdminMenu from '../../components/AdminMenu';
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  //getall users
+  const getAllUsers = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/user/all-users");
+      console.log(data);
+      setUsers(data.users);
+    } catch (error) {
+      console.log(error);
+      toast.error("Someething Went Wrong");
+    }
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
     <Layout title={"Dashboard - All Users"}>
-        <div className="container-fluid m-3 p-3">
-       <div className="row">
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
           <div className="col-md-3">
             <AdminMenu />
-           </div>
-           <div className="col-md-9">
-             <h1>All Users</h1>
+          </div>
+          <div className="col-md-9">
+            <h1>All Users</h1>
+            <div className="d-flex flex-wrap">
+              {users?.map((u) => (
+                  <div className="card m-2" style={{ width: "18rem" }} key={u._id}>
+                    <div className="card-body">
+                      <h5 className="card-title">{u.name}</h5>
+                      <p className="card-text">{"Email: " + u.email}</p>
+                      <p className="card-text">{"Address: " + u.address}</p>
+                      <p className="card-text">{"Role: " + u.role === 1 ? "Admin" : "User"}</p>
+                    </div>
+                  </div>
+              ))}
             </div>
+          </div>
         </div>
-        </div> 
+      </div>
     </Layout>
   );
 };
