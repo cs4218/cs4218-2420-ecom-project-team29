e6844dfac1754e543eab74a88924e3fd2a758faa
pzl111 <phuazailian@gmail.com>
2025-03-08 13:48:49 +0800

    fix: call get-all-users api only when authorised with token

--- client/src/pages/admin/Users.js
+++ client/src/pages/admin/Users.js
@@ -3,9 +3,11 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import Layout from '../../components/Layout';
 import AdminMenu from '../../components/AdminMenu';
+import { useAuth } from '../../context/auth';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [auth, setAuth] = useAuth();
 
   //getall users
   const getAllUsers = async () => {
@@ -20,8 +22,8 @@ const Users = () => {
   };
 
   useEffect(() => {
-    getAllUsers();
-  }, []);
+    if (auth?.token) getAllUsers();
+  }, [auth?.token]);
 
   return (
     <Layout title={"Dashboard - All Users"}>
@@ -34,15 +36,15 @@ const Users = () => {
             <h1>All Users</h1>
             <div className="d-flex flex-wrap">
               {users?.map((u) => (
-                  <div className="card m-2" style={{ width: "18rem" }} key={u._id}>
-                    <div className="card-body">
-                      <h5 className="card-title">{u.name}</h5>
-                      <p className="card-text mb-0">{"Email: " + u.email}</p>
-                      <p className="card-text mb-0">{"Address: " + u.address}</p>
-                      <p className="card-text mb-0">{"Phone: " + u.phone}</p>
-                      <p className="card-text mb-0">{u.role === 1 ? "Role: Admin" : "Role: User"}</p>
-                    </div>
+                <div className="card m-2" style={{ width: "18rem" }} key={u._id}>
+                  <div className="card-body">
+                    <h5 className="card-title">{u.name}</h5>
+                    <p className="card-text mb-0">{"Email: " + u.email}</p>
+                    <p className="card-text mb-0">{"Address: " + u.address}</p>
+                    <p className="card-text mb-0">{"Phone: " + u.phone}</p>
+                    <p className="card-text mb-0">{u.role === 1 ? "Role: Admin" : "Role: User"}</p>
                   </div>
+                </div>
               ))}
             </div>
           </div>
