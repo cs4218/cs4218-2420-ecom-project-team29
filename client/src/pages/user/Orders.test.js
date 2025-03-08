08f62b150ed2532391489d797f212fd2422b180d
elaineshijie <122242919+elaineshijie@users.noreply.github.com>
2025-03-08 19:39:12 +0800

    Fix ordersModel

--- client/src/pages/user/Orders.test.js
+++ client/src/pages/user/Orders.test.js
@@ -5,6 +5,7 @@ import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
 import Orders from "../../pages/user/Orders";
 import axios from "axios";
+import exp from "constants";
 
 // Mock the required modules
 jest.mock("axios");
@@ -47,12 +48,37 @@ describe("Orders Component", () => {
       _id: "Order2",
       status: "Not Processed",
       buyer: { name: "Green" },
-      createdAt: new Date().toISOString(),
+      createdAt: new Date().toISOString() - 1000,
       payment: { success: false },
       products: [
         {
           _id: "Product2",
           name: "Test Product Name 2",
+          description:
+            "Testing Product Description 2 that is realllllllyyy supppppppperrrr duppppppperrrr longggggggggggg !!! 1123456789 !@#$%^&*(){}|:",
+          price: 200,
+        },
+      ],
+    },
+  ];
+
+  const mockOrdersMoreThanOneProduct = [
+    {
+      _id: "Order1",
+      status: "Not Processed",
+      buyer: { name: "Green" },
+      createdAt: new Date().toISOString(),
+      payment: { success: true },
+      products: [
+        {
+          _id: "Product1",
+          name: "First Product",
+          description: "Testing Product Description",
+          price: 100,
+        },
+        {
+          _id: "Product2",
+          name: "Second Product",
           description: "Testing Product Description 2",
           price: 200,
         },
@@ -72,18 +98,33 @@ describe("Orders Component", () => {
     jest.clearAllMocks();
   });
 
-  it("should fetch and display data correctly", async () => {
+  it("should fetch and display data correctly for 1 order and multiple products in an order", async () => {
+    axios.get.mockResolvedValueOnce({ data: mockOrdersMoreThanOneProduct });
     render(
       <MemoryRouter>
         <Orders />
       </MemoryRouter>
     );
 
-    // Test that axios call to backend has been made
-    expect(axios.get).toHaveBeenCalledWith("/api/v1/auth/orders");
-
-    // Check if order data is displayed
     await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledWith("/api/v1/auth/orders");
+      expect(screen.getByText("Not Processed")).toBeInTheDocument(); // Check Status
+      expect(screen.getByText("Green")).toBeInTheDocument(); // Buyer name
+      expect(screen.getByText("Success")).toBeInTheDocument(); // Payment status
+      expect(screen.getByText("First Product")).toBeInTheDocument(); // Product name
+      expect(screen.getByText("Second Product")).toBeInTheDocument(); // 2nd Product name
+    });
+  });
+
+  it("should fetch and display data correctly for more than 1 order", async () => {
+    render(
+      <MemoryRouter>
+        <Orders />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledWith("/api/v1/auth/orders");
       expect(screen.getAllByText("Not Processed").length).toBe(2); // Check Status
       expect(screen.getAllByText("Green").length).toBe(2); // Buyer name
       expect(screen.getByText("Success")).toBeInTheDocument(); // Payment status
@@ -93,8 +134,20 @@ describe("Orders Component", () => {
     });
   });
 
+  // should test whether the headers are displayed correctly
+  it("should display headers correctly", async () => {
+    render(
+      <MemoryRouter>
+        <Orders />
+      </MemoryRouter>
+    );
+    await waitFor(() => {
+      expect(screen.getByText("All Orders")).toBeInTheDocument();
+    }
+    );
+  });
+
   it("should have no table when no orders but user is authenticated", async () => {
-    // Override the axios mock for this test
     axios.get.mockResolvedValueOnce({ data: [] });
 
     render(
@@ -112,7 +165,7 @@ describe("Orders Component", () => {
 
   it("should console.log when there is an API error", async () => {
     // Mock console.log to check for error logging
-    const logSpy = jest.spyOn(console, 'log');
+    const logSpy = jest.spyOn(console, "log");
 
     // Override the axios mock to throw an error
     const error = new Error("API Error");
@@ -127,13 +180,11 @@ describe("Orders Component", () => {
     expect(axios.get).toHaveBeenCalledWith("/api/v1/auth/orders");
     await waitFor(() => expect(console.log).toHaveBeenCalledWith(error));
 
-
     expect(logSpy).toBeCalledWith(error);
     logSpy.mockRestore();
   });
 
   it("should not fetch orders when user is not authenticated", async () => {
-    // Override auth mock to return no token
     const { useAuth } = require("../../context/auth");
     useAuth.mockReturnValueOnce([{}, jest.fn()]);
 
