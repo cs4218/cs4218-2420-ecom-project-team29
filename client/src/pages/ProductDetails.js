d7207ad0e74f764758274fc71140f9bfd85f8ba7
pzl111 <phuazailian@gmail.com>
2025-03-10 14:53:34 +0800

    fix: load image only when product id present, fix cosmetic issues

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -41,27 +41,28 @@ const ProductDetails = () => {
     <Layout>
       <div className="row container product-details">
         <div className="col-md-6">
-          <img
-            src={`/api/v1/product/product-photo/${product._id}`}
-            className="card-img-top"
-            alt={product.name}
-            height="300"
-            width={"350px"}
-          />
+          {product._id &&
+            <img
+              src={`/api/v1/product/product-photo/${product._id}`}
+              className="card-img-top"
+              alt={product.name}
+              height="300"
+              width={"350px"}
+            />
+          }
         </div>
         <div className="col-md-6 product-details-info">
           <h1 className="text-center">Product Details</h1>
           <hr />
-          <h6>Name : {product.name}</h6>
-          <h6>Description : {product.description}</h6>
+          <h6>Name: {product.name}</h6>
+          <h6>Description: {product.description}</h6>
           <h6>
-            Price :
-            {product?.price?.toLocaleString("en-US", {
+            Price: {product?.price?.toLocaleString("en-US", {
               style: "currency",
               currency: "USD",
             })}
           </h6>
-          <h6>Category : {product?.category?.name}</h6>
+          <h6>Category: {product?.category?.name}</h6>
           <button className="btn btn-secondary ms-1">ADD TO CART</button>
         </div>
       </div>
