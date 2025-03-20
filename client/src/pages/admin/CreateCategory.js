904ba6c7348ee0c9be90e343d206a89b11e5221f
pzl111 <phuazailian@gmail.com>
2025-03-20 22:36:44 +0800

    fix: styling

--- client/src/pages/admin/CreateCategory.js
+++ client/src/pages/admin/CreateCategory.js
@@ -101,7 +101,7 @@ const CreateCategory = () => {
   };
   return (
     <Layout title={"Dashboard - Create Category"} reload_categories={reload_categories}>
-      <div className="container-fluid m-3 p-3">
+      <div className="container-fluid p-5">
         <div className="row">
           <div className="col-md-3">
             <AdminMenu />
