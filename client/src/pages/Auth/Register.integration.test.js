090982c412c1a4ca8dc00d52f550bc43cadfbd6c
jonasgwt <jongoh2000@gmail.com>
2025-03-23 22:24:10 +0800

    fix test

--- client/src/pages/Auth/Register.integration.test.js
+++ client/src/pages/Auth/Register.integration.test.js
@@ -61,7 +61,6 @@ describe('Register Component', () => {
     fireEvent.change(screen.getByPlaceholderText('Enter Your Password'), { target: { value: 'password123' } });
     fireEvent.change(screen.getByPlaceholderText('Enter Your Phone'), { target: { value: '1234567890' } });
     fireEvent.change(screen.getByPlaceholderText('Enter Your Address'), { target: { value: '123 Street' } });
-    fireEvent.change(screen.getByPlaceholderText('Enter Your DOB'), { target: { value: '2000-01-01' } });
     fireEvent.change(screen.getByPlaceholderText('What is Your Favorite sports'), { target: { value: 'Football' } });
 
     fireEvent.click(screen.getByText('REGISTER'));
