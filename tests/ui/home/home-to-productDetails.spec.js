2f3c068850fd6583d7223b74697574a3aa207375
zengzihui <zeng_zihui@outlook.com>
2025-03-24 07:47:27 +0800

    feat: add UI testing for home page interaction with product details page

--- tests/ui/home/home-to-productDetails.spec.js
+++ tests/ui/home/home-to-productDetails.spec.js
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import productModel from '../../../models/productModel';
 
 dotenv.config();
 
@@ -16,26 +14,28 @@ test.describe('HomePage', () => {
 
     test.describe.configure({ mode: 'serial' });
 
-    // test.beforeAll(async ( page ) => {
-    //     // await mongoose.connect(process.env.MONGO_URL_TEST, { useNewUrlParser: true, useUnifiedTopology: true });
-    // });
-
-    // test.afterAll(async () => {
-    //     await mongoose.disconnect();
-    // });
-
     test.beforeEach(async ({ page }) => {
         await page.goto('http://localhost:3000');
         await expect(page).toHaveTitle('All Products - Best offers');
 
     });
 
-    test('', async ({ page }) => {
-        await page.goto('http://localhost:3000/');
-  
-        await expect(page).toHaveTitle('All Products - Best offers');
-
+    test('check if user can go to product details page and add to cart without logging in', async ({ page }) => {
+        await page.locator('.card-name-price > button').first().click();
+        await page.getByRole('button', { name: 'ADD TO CART' }).click();
+        await expect(page.locator('div').filter({ hasText: /^Please log in to add items to the cart$/ }).nth(2)).toBeVisible();
     });
-   
+
+    test('check if user can go to product details page and access the similar products page', async ({ page }) => {
+        await page.locator('.card-name-price > button').first().click();
+        await expect(page.getByRole('heading', { name: 'Name: 13-inch MacBook Air -' })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Similar Products ➡️' })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Wisepad 3 Reader' })).toBeVisible();
+        await page.getByRole('button', { name: 'More Details' }).click();
+        await expect(page.getByRole('heading', { name: 'Name: Wisepad 3 Reader' })).toBeVisible();
+        await expect(page.getByRole('heading', { name: 'Description: Use this card' })).toBeVisible();
+    });
+
+    
 });
 
