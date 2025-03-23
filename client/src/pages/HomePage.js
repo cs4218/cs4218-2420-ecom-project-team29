287eb2c2df67fb90b0d645a864daf223737a6af0
zengzihui <zeng_zihui@outlook.com>
2025-03-23 22:19:48 +0800

    fix: double rendering issue during category filtering

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -88,11 +88,9 @@ const HomePage = () => {
     }
     setChecked(all);
   };
+  
   useEffect(() => {
-    if (!checked.length || !radio.length) getAllProducts();
-  }, [checked.length, radio.length]);
-
-  useEffect(() => {
+    if (!checked.length && !radio.length) getAllProducts();
     if (checked.length || radio.length) filterProduct();
   }, [checked, radio]);
 
