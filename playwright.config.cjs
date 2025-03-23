65644163eb89165722455e2b1c68e36e7f1bf256
zengzihui <zeng_zihui@outlook.com>
2025-03-24 07:56:01 +0800

    Change back to grp config for playwright tests

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -11,9 +11,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  // testDir: './tests/ui',
-  testDir: './tests/ui/home',
-
+  testDir: './tests/ui',
 
   /* Run tests in files in parallel */
   fullyParallel: true,
